ebcbc81f2bb5791b039b46e566f9fe503b1b6528
Óskar <70016093+oskard95@users.noreply.github.com>
2026-01-07 11:07:45 +0100

    Update _main_.R
    Fixed a missing population script call

--- src/_main_.R
+++ src/_main_.R
@@ -9,7 +9,7 @@ library(logger)
 
 # Run only if no recent downloaded data is available, because downloads take long time
 #Population
-source(file.path(here(), "src", "dk/get_dk_yearly_counts.R"), encoding = 'UTF-8', print.eval = TRUE)
+source(file.path(here(), "src", "dk/get_dk_yearly_pop.R"), encoding = 'UTF-8', print.eval = TRUE)
 #Births
 source(file.path(here(), "src", "dk/get_dk_yearly_births.R"), encoding = 'UTF-8', print.eval = TRUE) 
 #Mortality
@@ -26,3 +26,4 @@ source(file.path(here(), "src", "dk/get_dk_yearly_emigration.R"), encoding = 'UT
 source(file.path(here(), "src", "prep_dk_data.R"), encoding = 'UTF-8', print.eval = TRUE)
 
 rm(list = ls())
+
